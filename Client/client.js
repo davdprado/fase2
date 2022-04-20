7e4b0c3b7d07fbc6b70eaad36457d835dd5c3c1d
David <3006692160101@ingenieria.usac.edu.gt>
2022-04-19 23:14:20 -0600

    cambios

--- Client/client.js
+++ Client/client.js
@@ -1,10 +1,14 @@
-const GRPCClient = require('node-grpc-client')
+const GRPCClient = require("node-grpc-client");
 
-const path = require("path")
+const path = require("path");
 
-const PROTO_PATH = path.resolve(__dirname, '../proto/test.proto')
+const PROTO_PATH = path.resolve(__dirname, "../proto/test.proto");
 
-var client = new GRPCClient(PROTO_PATH, 'main', 'testApi', '0.0.0.0:8080')
-
+var client = new GRPCClient(
+  PROTO_PATH,
+  "main",
+  "testApi",
+  "34.135.125.140:8080"
+);
 
 module.exports = client;
